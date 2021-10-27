1aaf17ca687650f59c94a3742f0cdf62c3cff74d
mihdan <mikhail@kobzarev.com>
2021-10-27 23:35:25 +0300

    Update plugin structure

--- mihdan-index-now.php
+++ mihdan-index-now.php
@@ -6,50 +6,11 @@
 
 namespace Mihdan\IndexNow;
 
-use WP_Post;
-
 /**
  * @link https://yandex.ru/support/webmaster/indexnow/key.html
  */
 define( 'MIHDAN_INDEX_NOW_KEY' , 'you_key_was_here' );
 
-add_action(
-    'transition_post_status',
-	/**
-	 * Fires actions related to the transitioning of a post's status.
-	 *
-	 * @param string  $new_status Transition to this post status.
-	 * @param string  $old_status Previous post status.
-	 * @param WP_Post $post       Post data.
-	 *
-	 * @link https://yandex.ru/dev/webmaster/doc/dg/reference/host-recrawl-post.html
-	 */
-    function ( $new_status, $old_status, WP_Post $post ) {
-	    // Срабатывает только на статус publish.
-	    if ( 'publish' !== $new_status || 'publish' === $old_status || ! in_array( $post->post_type, [ 'post', 'page' ] ) ) {
-		    return;
-	    }
+require_once __DIR__ . '/src/class-main.php';
 
-	    ping_with_yandex( $post );
-    },
-    10,
-    3
-);
-
-function ping_with_yandex( WP_Post $post ) {
-
-	$url = 'https://yandex.com/indexnow';
-	$args = array(
-		'timeout' => 30,
-		'body' => json_encode(
-			array(
-        'host'        => parse_url( get_home_url(), PHP_URL_HOST ),
-        'key'         => MIHDAN_INDEX_NOW_KEY,
-        'keyLocation' => '',
-				'urlList'     => [ get_permalink( $post->ID ) ]
-			)
-		),
-	);
-
-	$response = wp_remote_post( $url, $args );
-}
+( new Main() )->setup_hooks();
---
+++ src/class-main.php
@@ -0,0 +1,46 @@
+<?php
+namespace Mihdan\IndexNow;
+
+use WP_Post;
+
+class Main {
+	public function setup_hooks() {
+		add_action( 'transition_post_status', [ $this, 'maybe_do_pings' ], 10, 3 );
+	}
+
+	/**
+	 * Fires actions related to the transitioning of a post's status.
+	 *
+	 * @param string  $new_status Transition to this post status.
+	 * @param string  $old_status Previous post status.
+	 * @param WP_Post $post       Post data.
+	 *
+	 * @link https://yandex.ru/dev/webmaster/doc/dg/reference/host-recrawl-post.html
+	 */
+	public function maybe_do_pings( $new_status, $old_status, WP_Post $post ) {
+		// Срабатывает только на статус publish.
+		if ( 'publish' !== $new_status || 'publish' === $old_status || ! in_array( $post->post_type, [ 'post', 'page' ] ) ) {
+			return;
+		}
+
+		$this->ping_with_yandex( $post );
+	}
+
+	private function ping_with_yandex( WP_Post $post ) {
+
+		$url = 'https://yandex.com/indexnow';
+		$args = array(
+			'timeout' => 30,
+			'body' => json_encode(
+				array(
+					'host'        => parse_url( get_home_url(), PHP_URL_HOST ),
+					'key'         => MIHDAN_INDEX_NOW_KEY,
+					'keyLocation' => '',
+					'urlList'     => [ get_permalink( $post->ID ) ]
+				)
+			),
+		);
+
+		$response = wp_remote_post( $url, $args );
+	}
+}
