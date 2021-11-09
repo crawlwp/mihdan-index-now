f95121f8d3dea0912c03f2a2965625d73305f759
mihdan <mikhail@kobzarev.com>
2021-11-09 23:00:10 +0300

    Bump up version

--- mihdan-index-now.php
+++ mihdan-index-now.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Mihdan: Index Now
  * Description: Плагин уведомлений поисковых систем Яндекс/Google/Bing/Cloudflare о появлении новых страниц на сайте по протоколу IndexNow.
- * Version: 1.1.5
+ * Version: 1.2.0
  * Author: Mikhail Kobzarev
  * Author URI: https://www.kobzarev.com/
  * Plugin URI: https://wordpress.org/plugins/mihdan-index-now/
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'MIHDAN_INDEX_NOW_VERSION', '1.1.5' );
+define( 'MIHDAN_INDEX_NOW_VERSION', '1.2.0' );
 define( 'MIHDAN_INDEX_NOW_SLUG', 'mihdan-index-now' );
 define( 'MIHDAN_INDEX_NOW_PREFIX', 'mihdan_index_now' );
 define( 'MIHDAN_INDEX_NOW_NAME', 'IndexNow' );
@@ -33,11 +33,15 @@ $logger->setup_hooks();
 $wposa = new WPOSA(
 	MIHDAN_INDEX_NOW_NAME,
 	MIHDAN_INDEX_NOW_VERSION,
-	MIHDAN_INDEX_NOW_SLUG
+	MIHDAN_INDEX_NOW_SLUG,
+	MIHDAN_INDEX_NOW_PREFIX
 );
 $wposa->setup_hooks();
 
-$settings = new Settings( $wposa );
+$help_tab = new HelpTab();
+$help_tab->setup_hooks();
+
+$settings = new Settings( $wposa, $help_tab );
 $settings->setup_hooks();
 
 ( new Main( $logger, $settings ) )->setup_hooks();
