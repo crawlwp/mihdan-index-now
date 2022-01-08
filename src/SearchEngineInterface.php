1726a4c04c2014380179fe34e0ab86e5fd9eeb3f
mihdan <mikhail@kobzarev.com>
2022-01-09 02:42:13 +0300

    Add required methods for SE interface

--- src/SearchEngineInterface.php
+++ src/SearchEngineInterface.php
@@ -10,4 +10,6 @@ namespace Mihdan\IndexNow;
 interface SearchEngineInterface {
 	public function get_slug(): string;
 	public function get_name(): string;
+	public function setup_hooks();
+	public function is_enabled(): bool;
 }
