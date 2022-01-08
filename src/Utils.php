1be1ec62136010af54563169aaf27361d69c803d
mihdan <mikhail@kobzarev.com>
2022-01-09 02:40:21 +0300

    Add custom Utils methods

--- src/Utils.php
+++ src/Utils.php
@@ -12,6 +12,15 @@ class Utils {
 	}
 
 	/**
+	 * Get plugin basename.
+	 *
+	 * @return string
+	 */
+	public static function get_plugin_basename() {
+		return MIHDAN_INDEX_NOW_BASENAME;
+	}
+
+	/**
 	 * Get plugin vesrion.
 	 *
 	 * @return string
@@ -54,5 +63,18 @@ class Utils {
 	 */
 	public static function get_plugin_name() {
 		return MIHDAN_INDEX_NOW_NAME;
+	}
+
+	public static function is_response_code_success( $code ): bool {
+		return ( $code >= 200 && $code < 300 );
+	}
+
+	/**
+	 * Get user agent of browser/bot.
+	 *
+	 * @return mixed|string
+	 */
+	public static function get_user_agent(): string {
+		return wp_unslash( $_SERVER['HTTP_USER_AGENT'] ?? '' );
 	}
 }
