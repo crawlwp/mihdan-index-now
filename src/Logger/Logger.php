65c4e1efa1c0fb3201f2742482823b4eb6de1f67
mihdan <mikhail@kobzarev.com>
2022-01-09 01:00:11 +0300

    Remove log entries on post removing.

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -2,11 +2,28 @@
 namespace Mihdan\IndexNow\Logger;
 
 use Psr\Log\AbstractLogger;
+use WP_Post;
 
 class Logger extends AbstractLogger {
 	public function get_logger_table_name() {
 		global $wpdb;
 		return $wpdb->prefix . 'index_now_log';
+	}
+
+	public function setup_hooks() {
+		add_action( 'after_delete_post', [ $this, 'maybe_delete_log_entries' ], 10, 2 );
+	}
+
+	/**
+	 * Delete log entries fired when on post delete.
+	 *
+	 * @param int     $post_id Post ID.
+	 * @param WP_Post $post    Instance of WP_Post.
+	 */
+	public function maybe_delete_log_entries( int $post_id, WP_Post $post ) {
+		global $wpdb;
+
+		$wpdb->delete( $this->get_logger_table_name(), [ 'post_id' => $post_id ] );
 	}
 
 	public function log( $level, $message, array $context = [] ) {
