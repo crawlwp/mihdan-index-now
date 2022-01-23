f462b77b4f92fc6385a52dea8757ef0efd8cc727
mihdan <mikhail@kobzarev.com>
2022-01-24 00:40:29 +0300

    Improve uninstall process

--- uninstall.php
+++ uninstall.php
@@ -17,5 +17,10 @@ global $wpdb;
 $wpdb->query( "DROP TABLE {$wpdb->prefix}index_now_log" ); // phpcs:ignore
 
 // Delete settings.
-delete_option( 'mihdan_index_now_contacts' );
 delete_option( 'mihdan_index_now_general' );
+delete_option( 'mihdan_index_now_index_now' );
+delete_option( 'mihdan_index_now_bing_webmaster' );
+delete_option( 'mihdan_index_now_google_webmaster' );
+delete_option( 'mihdan_index_now_yandex_webmaster' );
+delete_option( 'mihdan_index_now_logs' );
+delete_option( 'mihdan_index_now_version' );
