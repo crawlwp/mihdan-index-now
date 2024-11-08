f7941af0040b2a01cf072967e4dd8f051c495c1a
Collins Agbonghama <collizo4sky@gmail.com>
2024-11-08 12:07:19 +0100

    worked

--- plugin-build.php
+++ plugin-build.php
@@ -25,34 +25,34 @@ $chdir = chdir($plugin_git_folder);
 if ( ! $chdir) exit;
 
 exec('composer install --no-dev --no-progress --no-suggest');
-//exec('composer prefix-dependencies');
-//
-//deleteDir('.git');
-//deleteDir('.github');
-//deleteDir('.wordpress-org');
-//deleteDir('php-scoper');
-//deleteDir('codekit');
-//
-//deleteDir('vendor');
-//
-//foreach (
-//    array(
-//        '.gitignore',
-//        '.distignore',
-//        '.editorconfig',
-//        'scoper.inc.php',
-//        'composer.json',
-//        'README.md',
-//        'plugin-build.php',
-//        'composer.lock',
-//        'mihdan-index-now.zip',
-//    ) as $file
-//) {
-//    @unlink($file);
-//}
-//
-//// move up directory
-//chdir($dir);
-//exec("7z a mihdan-index-now.zip mihdan-index-now/");
-//deleteDir($plugin_git_name);
+exec('composer prefix-dependencies');
+
+deleteDir('.git');
+deleteDir('.github');
+deleteDir('.wordpress-org');
+deleteDir('php-scoper');
+deleteDir('codekit');
+
+deleteDir('vendor');
+
+foreach (
+    array(
+        '.gitignore',
+        '.distignore',
+        '.editorconfig',
+        'scoper.inc.php',
+        'composer.json',
+        'README.md',
+        'plugin-build.php',
+        'composer.lock',
+        'mihdan-index-now.zip',
+    ) as $file
+) {
+    @unlink($file);
+}
+
+// move up directory
+chdir($dir);
+exec("7z a mihdan-index-now.zip mihdan-index-now/");
+deleteDir($plugin_git_name);
 
