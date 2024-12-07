27423a077f8845bf02cee5f69f461dc0181bfbc5
Collins Agbonghama <collizo4sky@gmail.com>
2024-12-07 12:43:50 +0100

    Added baidu

--- src/SEOCore/SiteVerification/SiteVerificationSettings.php
+++ src/SEOCore/SiteVerification/SiteVerificationSettings.php
@@ -16,46 +16,65 @@ class SiteVerificationSettings
 
 	public function core_settings_fields(WPOSA $wposa, $settingsInstance)
 	{
-		if ($wposa->get_active_header_menu() == Utils::get_plugin_prefix() . '_core_settings') {
+		if ($wposa->get_active_header_menu() === Utils::get_plugin_prefix() . '_core_settings') {
 
 			$wposa->add_section([
 				'header_menu_id' => 'core_settings',
 				'id'             => 'site_verification',
 				'title'          => __('Site Verification', 'mihdan-index-now'),
-				'desc'           => esc_html__('To verify your website with tools such as Google Search Console, Bing Webmaster Tools, and Yandex Webmaster Tools, you need to add a verification meta tag to your site. These options will help you seamlessly integrate the required codes.', 'mihdan-index-now')
+				'desc'           => esc_html__('To verify your website with tools such as Google Search Console, Bing Webmaster Tools, and Yandex Webmaster Tools, you need to add a verification meta tag to your site. These options will help you seamlessly integrate the required codes.', 'mihdan-index-now'),
 			]);
 
-			$wposa->add_field(
-				'site_verification',
-				[
-					'id'       => 'google',
-					'type'     => 'text',
+			$providers = [
+				'google' => [
 					'name'     => __('Google Verification Code', 'mihdan-index-now'),
-					'desc'     => '<code>' . sprintf(esc_html('<meta name="google-site-verification" content="%ssite-verification-code%s"/>'), '<span style="color:#a11">', '</strong>') . '</code>',
+					'meta_tag' => '<meta name="google-site-verification" content="%ssite-verification-code%s"/>',
 					'help_tab' => 'https://crawlwp.com/?p=763&utm_source=wp_dashboard&utm_medium=site_verification_page&utm_campaign=google',
-				]
-			);
-
-			$wposa->add_field(
-				'site_verification',
-				[
-					'id'       => 'bing',
-					'type'     => 'text',
+				],
+				'bing'   => [
 					'name'     => __('Bing Verification Code', 'mihdan-index-now'),
-					'desc'     => '<code>' . sprintf(esc_html('<meta name="msvalidate.01" content="%ssite-verification-code%s" />'), '<span style="color:#a11">', '</strong>') . '</code>',
-					'help_tab' => 'https://crawlwp.com/?p=763&utm_source=wp_dashboard&utm_medium=site_verification_page&utm_campaign=bing',
-				]
-			);
+					'meta_tag' => '<meta name="msvalidate.01" content="%ssite-verification-code%s" />',
+					'help_tab' => 'https://crawlwp.com/?p=768&utm_source=wp_dashboard&utm_medium=site_verification_page&utm_campaign=bing',
+				],
+				'yandex' => [
+					'name'     => __('Yandex Verification Code', 'mihdan-index-now'),
+					'meta_tag' => '<meta name="yandex-verification" content="%ssite-verification-code%s" />',
+					'help_tab' => 'https://crawlwp.com/?p=770&utm_source=wp_dashboard&utm_medium=site_verification_page&utm_campaign=yandex',
+				],
+				'baidu' => [
+					'name'     => __('Baidu Verification Code', 'mihdan-index-now'),
+					'meta_tag' => '<meta name="baidu-site-verification" content="%ssite-verification-code%s" />',
+					'help_tab' => 'https://crawlwp.com/?p=772&utm_source=wp_dashboard&utm_medium=site_verification_page&utm_campaign=baidu',
+				],
+			];
+
+			foreach ($providers as $id => $provider) {
+				$wposa->add_field(
+					'site_verification',
+					[
+						'id'       => $id,
+						'type'     => 'text',
+						'name'     => $provider['name'],
+						'desc'     => '<code>' . sprintf(esc_html($provider['meta_tag']), '<span style="color:#a11">', '</strong>') . '</code>',
+						'help_tab' => $provider['help_tab'],
+					]
+				);
+			}
 		}
 	}
 
+
 	public function sanitize_site_verification_data($submitted_data, $name)
 	{
-		if ($name == 'mihdan_index_now_site_verification' && isset($submitted_data['google'])) {
-			$regex = '/<meta.+content=(?:"|\')(.+)(?:"|\').+/';
+		if ($name === 'mihdan_index_now_site_verification') {
+			$regex     = '/<meta.+content=(?:"|\')(.+)(?:"|\').+/';
+			$providers = ['google', 'bing', 'yandex'];
 
-			$submitted_data['google'] = preg_replace($regex, '$1', wp_unslash($submitted_data['google']));
-			$submitted_data['bing']   = preg_replace($regex, '$1', wp_unslash($submitted_data['bing']));
+			foreach ($providers as $provider) {
+				if (isset($submitted_data[$provider])) {
+					$submitted_data[$provider] = preg_replace($regex, '$1', wp_unslash($submitted_data[$provider]));
+				}
+			}
 		}
 
 		return $submitted_data;
