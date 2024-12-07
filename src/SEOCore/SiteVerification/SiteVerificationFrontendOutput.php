c2545dccbf0a05b158b7ede34f0437f9c826ec2a
Collins Agbonghama <collizo4sky@gmail.com>
2024-12-07 14:30:44 +0100

    Finito

--- src/SEOCore/SiteVerification/SiteVerificationFrontendOutput.php
+++ src/SEOCore/SiteVerification/SiteVerificationFrontendOutput.php
@@ -15,30 +15,33 @@ class SiteVerificationFrontendOutput
 	{
 		if (is_home() || is_front_page()) {
 
-			$google_verification_code    = Utils::get_setting_data('site_verification', 'google');
-			$bing_verification_code      = Utils::get_setting_data('site_verification', 'bing');
-			$yandex_verification_code    = Utils::get_setting_data('site_verification', 'yandex');
-			$baidu_verification_code     = Utils::get_setting_data('site_verification', 'baidu');
-			$pinterest_verification_code = Utils::get_setting_data('site_verification', 'pinterest');
+			$verification_data = [
+				'google' => [
+					'meta_name' => 'google-site-verification',
+					'value'     => Utils::get_setting_data('site_verification', 'google')
+				],
+				'bing' => [
+					'meta_name' => 'msvalidate.01',
+					'value'     => Utils::get_setting_data('site_verification', 'bing')
+				],
+				'yandex' => [
+					'meta_name' => 'yandex-verification',
+					'value'     => Utils::get_setting_data('site_verification', 'yandex')
+				],
+				'baidu' => [
+					'meta_name' => 'baidu-site-verification',
+					'value'     => Utils::get_setting_data('site_verification', 'baidu')
+				],
+				'pinterest' => [
+					'meta_name' => 'p:domain_verify',
+					'value'     => Utils::get_setting_data('site_verification', 'pinterest')
+				],
+			];
 
-			if ( ! empty($google_verification_code)) {
-				echo sprintf('<meta name="google-site-verification" content="%s" />', esc_attr($google_verification_code));
-			}
-
-			if ( ! empty($bing_verification_code)) {
-				echo sprintf('<meta name="msvalidate.01" content="%s" />', esc_attr($bing_verification_code));
-			}
-
-			if ( ! empty($yandex_verification_code)) {
-				echo sprintf('<meta name="yandex-verification" content="%s" />', esc_attr($yandex_verification_code));
-			}
-
-			if ( ! empty($baidu_verification_code)) {
-				echo sprintf('<meta name="baidu-site-verification" content="%s" />', esc_attr($baidu_verification_code));
-			}
-
-			if ( ! empty($pinterest_verification_code)) {
-				echo sprintf('<meta name="p:domain_verify" content="%s" />', esc_attr($pinterest_verification_code));
+			foreach ($verification_data as $data) {
+				if ( ! empty($data['value'])) {
+					echo sprintf('<meta name="%s" content="%s" />%s', esc_attr($data['meta_name']), esc_attr($data['value']), PHP_EOL);
+				}
 			}
 		}
 	}
