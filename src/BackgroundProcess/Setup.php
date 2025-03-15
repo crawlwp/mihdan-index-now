11a420c9b7885632f874b22f289ba68fc8de94be
Collins Agbonghama <collizo4sky@gmail.com>
2025-03-15 15:16:25 +0100

    Created an alternate background processing that does not rely on non-blocking request

--- src/BackgroundProcess/Setup.php
+++ src/BackgroundProcess/Setup.php
@@ -11,6 +11,8 @@ class Setup extends WP_Background_Process
 
 	protected $cron_interval = 1;
 
+	protected $is_alternate_cron_runner = true;
+
 	public function __construct()
 	{
 		// Uses unique prefix per blog so each blog has separate queue.
@@ -21,6 +23,54 @@ class Setup extends WP_Background_Process
 		add_filter($this->identifier . '_seconds_between_batches', function ($seconds) {
 			return 1;
 		});
+	}
+
+	public function dispatch()
+	{
+		if (apply_filters('crawlwp_alternate_cron_runner', $this->is_alternate_cron_runner)) {
+			return $this->maybe_handle();
+		}
+
+		// Perform remote post.
+		return parent::dispatch();
+	}
+
+	public function maybe_handle()
+	{
+		// Don't lock up other requests while processing.
+		session_write_close();
+
+		if ($this->is_processing()) {
+			// Background process already running.
+			return $this->maybe_wp_die();
+		}
+
+		if ($this->is_cancelled()) {
+			$this->clear_scheduled_event();
+			$this->delete_all();
+
+			return $this->maybe_wp_die();
+		}
+
+		if ($this->is_paused()) {
+			$this->clear_scheduled_event();
+			$this->paused();
+
+			return $this->maybe_wp_die();
+		}
+
+		if ($this->is_queue_empty()) {
+			// No data to process.
+			return $this->maybe_wp_die();
+		}
+
+		if ( ! $this->is_alternate_cron_runner) {
+			check_ajax_referer($this->identifier, 'nonce');
+		}
+
+		$this->handle();
+
+		return $this->maybe_wp_die();
 	}
 
 	protected function task($item)
