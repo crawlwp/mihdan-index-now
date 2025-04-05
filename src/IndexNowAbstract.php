d74e74f10dadd687cd6a2ee2c24273a93bc25064
Collins Agbonghama <collizo4sky@gmail.com>
2025-04-05 17:46:23 +0100

    normalized post/term url so filter can be used to rewrite the url

--- src/IndexNowAbstract.php
+++ src/IndexNowAbstract.php
@@ -36,13 +36,6 @@ abstract class IndexNowAbstract implements SearchEngineInterface
 	private $wposa;
 
 	/**
-	 * Site host name.
-	 *
-	 * @var string $host
-	 */
-	private $host;
-
-	/**
 	 * Post types.
 	 *
 	 * @var array[]
@@ -65,7 +58,6 @@ abstract class IndexNowAbstract implements SearchEngineInterface
 	{
 		$this->logger     = $logger;
 		$this->wposa      = $wposa;
-		$this->host       = apply_filters('mihdan_index_now/host', wp_parse_url(Utils::normalized_home_url(), PHP_URL_HOST));
 		$this->post_types = apply_filters('mihdan_index_now/post_types', (array)$this->wposa->get_option('post_types', 'general', []));
 		$this->taxonomies = apply_filters('mihdan_index_now/taxonomies', (array)$this->wposa->get_option('taxonomies', 'general', []));
 		$this->api_key    = $this->wposa->get_option('api_key', 'index_now', Utils::generate_key());
@@ -174,7 +166,7 @@ abstract class IndexNowAbstract implements SearchEngineInterface
 	 */
 	private function get_host()
 	{
-		return $this->host;
+		return apply_filters('mihdan_index_now/host', wp_parse_url(Utils::normalized_home_url(), PHP_URL_HOST));
 	}
 
 	public function push(array $url_list): bool
